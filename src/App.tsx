efb3b3ae4b7f7f8ca5ca55faf5a11359e31a9d77
Federico Pessina <federicopessina.work@gmail.com>
2025-12-17 12:24:28 +0100

    feat: MVP

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Bytes</h1>
+      <p>My personal blog about code and ideas</p>
+    </div>
+  );
+}
+
+export default App
