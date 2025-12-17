acb342be7ee7b756abc1e5e5ad9260e3074d51c8
Federico Pessina <federicopessina.work@gmail.com>
2025-12-17 18:01:19 +0100

    Updates

---
+++ assets/hello-world-BEehX59r.js
@@ -0,0 +1,6 @@
+const o=`## Hello World\r
+\r
+Welcome to **Bytes**.\r
+\r
+This is my first post.\r
+`;export{o as default};
---
+++ assets/learning-react-DFgWBHiB.js
@@ -0,0 +1,3 @@
+const e=`## Learning React\r
+\r
+This blog is written in React + TypeScript + Vite as I'm learning front-end development`;export{e as default};
