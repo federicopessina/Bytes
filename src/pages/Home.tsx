3d91948cc820ac8912ac30b56e97a0bd94525d1c
federicopessina <federicopessina.work@gmail.com>
2026-08-04 21:35:06 +0200

    fix: resolve dependency in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import MarkdownPage from "../components/MarkdownPage";
-import home from "../content/pages/home.md?raw";
+import home from "../content/static/home.md?raw";
 
 export default function Home() {
   return <MarkdownPage content={home} />;
